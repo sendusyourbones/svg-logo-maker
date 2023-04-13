29039dbfbc790e818f5e1ee37fc18b16cb05134b
sendusyourbones <beckee.roos@gmail.com>
2023-04-12 21:21:05 -0700

    Add validation for inquirer prompts

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// Packages needed for this application
+// Packages / variables needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const validateInput = require('./lib/validateInput.js');
+const { validateText, validateColor } = validateInput;
 const shapes = require('./lib/shapes.js');
-const Triangle = shapes.Triangle;
-const Square = shapes.Square;
-const Circle = shapes.Circle;
+const { Triangle, Square, Circle } = shapes;
 
 // Function to write logo file logo.svg
 function writeToFile(fileName, data) {
@@ -19,49 +19,51 @@ function init() {
         {
             name: 'text',
             message: 'Enter up to three characters to include in the logo',
-            type: 'input'
+            type: 'input',
+            validate: validateText
         },
         {
             name: 'textColor',
             message: 'Enter the name or hexadecimal number of the desired text color for the logo',
-            type: 'input'
+            type: 'input',
+            validate: validateColor
         },
         {
             name: 'shape',
             message: 'What shape would you like the logo to be?',
             type: 'rawlist',
             choices: [
-                'Circle',
-                'Triangle',
-                'Square'
+                {
+                    name: 'Circle',
+                    value: new Circle()
+                },
+                {
+                    name: 'Triangle',
+                    value: new Triangle()
+                },
+                {
+                    name: 'Square',
+                    value: new Square()
+                }
             ]
         },
         {
             name: 'shapeColor',
             message: 'Enter the name or hexadecimal number of the desired color for the logo shape',
-            type: 'input'
+            type: 'input',
+            validate: validateColor
         }
     ]).then((response) => {
         const { text, textColor, shape, shapeColor } = response;
-
-        let newShape;
-
-        if (shape === 'Circle') {
-            newShape = new Circle();
-        } else if (shape === 'Triangle') {
-            newShape = new Triangle();
-        } else if (shape === 'Square') {
-            newShape = new Square();
-        }
         
-        newShape.setColor(shapeColor);
+        shape.setColor(shapeColor);
 
         const textForFile = 
 `<svg version="1.1" 
 width="300" height="200" 
 xmlns="http://www.w3.org/2000/svg">
-${newShape.render()}
-${newShape.renderText(textColor, text)}
+${shape.render()}
+${shape.renderText(textColor, text)}
 </svg>`
 
         writeToFile('logo.svg', textForFile);
